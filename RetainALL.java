f3cac24e6eac8dbe62c82a0713fb913b35e3a663
chaitud2006 <cc2214574@gmail.com>
2024-12-13 15:12:51 +0530

    Add files via upload

--- RetainALL.java
+++ RetainALL.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class RetainALL{
-    public static void main(String args[]){
+class RetainALL{
+    public static void main(String [] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int arr1[]=new int[n];
@@ -8,20 +8,20 @@ public class RetainALL{
             arr1[i]=s.nextInt();
         }
          int m=s.nextInt();
-        int arr2[]=new int[m];
+        int arr2[]=new int[n];
         for(int i=0;i<m;i++){
-            arr1[i]=s.nextInt();
+            arr2[i]=s.nextInt();
         }
         Set<Integer> a=new HashSet<>();
         Set<Integer> b=new HashSet<>();
-        for(int s2:arr1)
-        a.add(s2);
-        for(int s1:arr2)
-        b.add(s1);
+        for(int s1:arr1)
+            a.add(s1);
+        for(int s2:arr2)
+            b.add(s2);
         System.out.println(a);
         System.out.println(b);
-        Set<Integer> u=new HashSet<>(a);
-        u.retainAll(b);
-        System.out.println(u);
+        Set<Integer> c=new HashSet<>(a);
+        c.retainAll(b);
+        System.out.println(c);
     }
 }
